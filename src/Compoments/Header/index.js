ae6bddbbc963dd19cadaf9c974a153945d4bdba8
Ngocon2004 <buivhai2004@gmail.com>
2025-09-29 21:31:53 +0700

    pust

---
+++ src/Compoments/Header/index.js
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (   
+    <>
+        <div className="header Wrapper">
+            <div className="top-strip bg-blue">
+                <div className="container">
+                    <p className="mb-0 mt-0 text-center ">Shop Đồ Dùng Cho Thú Cưng</p>
+                </div>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default Header;
