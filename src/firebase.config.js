2294555da055af13768bbef6c1d3072c75bba24f
Ngocon2004 <buivhai2004@gmail.com>
2025-10-24 10:46:25 +0700

    firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+
+
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
@@ -8,16 +10,19 @@ import { getStorage } from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDpfvFHG7KPRe_nSDaB9l4qzIftbMznpQg",
+  authDomain: "studio-1834158531-ea15a.firebaseapp.com",
+  projectId: "studio-1834158531-ea15a",
+  storageBucket: "studio-1834158531-ea15a.firebasestorage.app",
+  messagingSenderId: "445404243155",
+  appId: "1:445404243155:web:ee24341f76ffe47c9aa026"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
